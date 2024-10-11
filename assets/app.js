bba995ca8af6ae0829bedd8cad6d2458d4f58cd2
Dalya <dalya.k.t.ahmed88@gmail.com>
2024-10-11 10:08:51 +0200

    update

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,10 @@
 
+//js to the försatta handla in the mini cart
+
+  // Close the cart drawer when the "Fortsätt handla" button is clicked
+  document.getElementById('CartDrawer-ContinueShopping').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent any default behavior (like a page refresh or scroll)
+    
+    // Find the cart-drawer element and close it
+    document.querySelector('cart-drawer').close();
+  });
